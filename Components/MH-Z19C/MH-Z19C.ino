ce780161a82d209b722cf7524ba695e8a7720cfb
Simon Schimik <simon.schimik@gmail.com>
2021-03-26 18:49:10 +0100

    finished dummy tft design, edited tft fritzing, added mh-z19c base code + fritzing

---
+++ Components/MH-Z19C/MH-Z19C.ino
@@ -0,0 +1,27 @@
+#include "MHZ19.h"                                        
+#include <SoftwareSerial.h> 
+
+#define RX_PIN 12                                          // Rx pin which the MHZ19 Tx pin is attached to
+#define TX_PIN 16                                          // Tx pin which the MHZ19 Rx pin is attached to
+#define BAUDRATE 9600                                      
+
+MHZ19 myMHZ19;                                            
+SoftwareSerial mySerial(RX_PIN, TX_PIN);     
+
+void setup() {
+  Serial.begin(9600);                                     
+  mySerial.begin(BAUDRATE);                               
+  myMHZ19.begin(mySerial);                               
+  myMHZ19.autoCalibration(); 
+  Serial.println("Preheating sensor!");
+  delay(120000);
+}
+
+void loop() {
+  int CO2; 
+  CO2 = myMHZ19.getCO2();
+  Serial.print("CO2 (ppm): ");                      
+  Serial.println(CO2);
+  
+  delay(2000);                                      
+}
